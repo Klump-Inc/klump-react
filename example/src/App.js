a3b521ac00b4f49062bdc10385fcf345174dd815
Kingsley Atanang <kingo4luv@gmail.com>
2024-11-29 09:42:13 +0100

    update version compatibilty

--- example/src/App.js
+++ example/src/App.js
@@ -2,42 +2,61 @@ import React, {useState} from 'react'
 
 import { KlumpCheckout } from 'klump-react'
 
+const products = [{
+    name: "Iphone 4",
+    price: 100000
+  },
+  {
+    name: "Iphone 5",
+    price: 200000
+  },
+  {
+    name: "Iphone 6",
+    price: 150000
+  },
+]
+
 const App = () => {
-   // eslint-disable-next-line no-unused-vars
-   const [data, setData] = useState(
-     {
-       amount: 4100,
-       shipping_fee: 100,
-       currency: 'NGN',
-       redirect_url: 'https://verygoodmerchant.com/checkout/confirmation',
-       merchant_reference: 'what-ever-you-want-this-to-be',
-       meta_data: {
-         customer: 'Elon Musk',
-         email: 'musk@spacex.com'
-       },
-       items: [{
-         image_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-         item_url: 'https://www.paypal.com/in/webapps/mpp/home',
-         name: 'Awesome item',
-         unit_price: '2000',
-         quantity: 2,
-       }]
-     }
-   )
+  
+
+   const [product, setProduct] = useState({})
+   const [productPrice, setProductPrice] = useState(0)
+   const [quantity, setQuantity] = useState(1)
+   const [name, setName] = useState("")
+   const [email, setEmail] = useState("")
 
     const payWithKlump = () => {
-        const payload = {
-           publicKey: 'klp_pk_1234abdc5678',
-           data,
-           onSuccess,
-           onError,
-           onLoad,
-           onOpen,
-           onClose
-        }
+        // You can run your validation checks here
+        const data = {
+          amount: productPrice * quantity,
+          currency: 'NGN',
+          meta_data: {
+            customer: name,
+            email: email
+          },
+          items: [{
+            name: product,
+            quantity,
+            unit_price: productPrice
+          }]
 
+        }
+       
         // eslint-disable-next-line no-undef
-        new Klump(payload)
+        new Klump({
+            publicKey: 'klp_pk_1234abdc5678',
+            data,
+            onSuccess,
+            onError,
+            onLoad,
+            onOpen,
+            onClose
+        })
+    }
+
+    const handleProductChange = (e) => {
+      setProduct(e.target.value)
+      setProductPrice(products.find(product => product.name === e.target.value).price)
     }
 
     const onSuccess = (data) => {
@@ -64,7 +83,46 @@ const App = () => {
       console.log(data);
     }
 
-  return <KlumpCheckout onClick={payWithKlump}/>
+  return (
+      <section>
+        <h1>Buy Now</h1>
+        <form className="container">
+            <div>
+                <label htmlFor="name">
+                    <strong>Full Name</strong>
+                </label>
+                <input onChange={(e) => setName(e.target.value)} required type="text" placeholder="Enter full name"  />
+            </div>
+            <div>
+                <label htmlFor="email">
+                <strong>Email Address</strong>
+                </label>
+                <input onChange={(e) => setEmail(e.target.value)} required type="text" value={email} placeholder="Enter email" name="email" />
+            </div>
+             <div>
+                <label htmlFor="product">
+                <strong>Product</strong>
+                </label>
+                <select onChange={handleProductChange} name="product" value={product} id="product">
+                  <option value="">Please select a product</option>
+                  {products.map(product => (
+                    <option key={product.name} value={product.name} >{product.name}</option>
+                  ))}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="quantity">
+                <strong>Quantity</strong>
+                </label>
+                <input onChange={(e) => setQuantity(e.target.value)} min="1"  type="number" value={quantity} placeholder="quantity" name="quantity" required />
+            </div>
+             <h2>Total: {productPrice * quantity}</h2>
+            <KlumpCheckout onClick={payWithKlump}/>
+        </form>
+    </section>
+  )
+  
+
 }
 
 export default App
