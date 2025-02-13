c0d727b9a127ee9306ae9b95159a004346a29186
Kingsley Atanang <kingo4luv@gmail.com>
2025-02-13 11:46:33 +0100

    made redeclare a conditional

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import React, { useEffect } from 'react'
 export const KlumpCheckout = ({ onClick }) => {
   useEffect(() => {
     const scriptSrc = 'https://js.useklump.com/klump.js'
-
     if (!document.querySelector(`script[src="${scriptSrc}"]`)) {
       const script = document.createElement('script')
       script.src = scriptSrc
       script.async = true
       document.body.appendChild(script)
-
-      return () => {
-        if (script.parentNode) {
-          script.parentNode.removeChild(script)
-        }
-      }
     }
   }, [])
   return <div id='klump__checkout' onClick={onClick} />
