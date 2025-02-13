78fdf19e8cd617bb1c5305c933e13ed702c2f2e0
Kingsley Atanang <kingo4luv@gmail.com>
2025-02-13 11:28:41 +0100

    fix redeclared script

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React, { useEffect } from 'react'
 
 export const KlumpCheckout = ({ onClick }) => {
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = `https://js.useklump.com/klump.js`
-    script.async = true
-    document.body.appendChild(script)
-    return () => {
-      document.body.removeChild(script)
+    const scriptSrc = 'https://js.useklump.com/klump.js'
+
+    if (!document.querySelector(`script[src="${scriptSrc}"]`)) {
+      const script = document.createElement('script')
+      script.src = scriptSrc
+      script.async = true
+      document.body.appendChild(script)
+
+      return () => {
+        if (script.parentNode) {
+          script.parentNode.removeChild(script)
+        }
+      }
     }
   }, [])
   return <div id='klump__checkout' onClick={onClick} />
